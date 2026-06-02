043c2bafe2245706fd50f41bd7fb5f9a56b6db1b
basedhardow-pixel <basedhardow@gmail.com>
2026-06-02 14:55:04 +0300

    fix duplicate messages

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ logging.basicConfig(level=logging.INFO)
 def main():
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message), group=1)
+    
     print("✅ Бот запущен!")
     app.run_polling()
 
